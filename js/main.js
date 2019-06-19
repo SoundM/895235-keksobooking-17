c8167905b6060ffc40a261231cb5be3e657668e4
SoundM <galina45192@gmail.com>
2019-06-19 20:35:13 +0200

    Валидация формы

--- js/main.js
+++ js/main.js
@@ -108,3 +108,79 @@ var getPinMainCoordinates = function (elementWidth, elementHeight) { // https://
 mapPinMain.addEventListener('mouseup', function () {
   getPinMainCoordinates(PIN_MAIN_WIDTH, PIN_MAIN_HEIGHT);
 });
+
+// Часть вторая
+
+var inputTitle = adForm.querySelector('#title');
+var inputPrice = adForm.querySelector('#price');
+var inputType = adForm.querySelector('#type');
+var inputTimeIn = adForm.querySelector('#timein');
+var inputTimeOut = adForm.querySelector('#timeout');
+
+
+// Валидация данных формы заголовка
+inputTitle.addEventListener('input', function (evt) {
+  var target = evt.target;
+  inputTitle.required = true;
+
+  if (target.value.length < 30) {
+    target.setCustomValidity('Заголовок объявления не должен быть короче 30 символов');
+  } else if (target.value.length > 100) {
+    target.setCustomValidity('Заголовок объявления не должен быть длиннее 100 символов');
+  } else {
+    target.setCustomValidity('');
+  }
+});
+
+// Валидация данных формы типа жилья
+inputPrice.addEventListener('input', function (evt) {
+  var target = evt.target;
+  target.required = true;
+  target.type = 'number';
+
+  if (inputType.value === 'bungalo' && target.value < 0) {
+    target.setCustomValidity('Цена за ночь не может быть ниже 0');
+  } else if (inputType.value === 'flat' && target.value < 1000) {
+    target.setCustomValidity('Цена за ночь не может быть ниже 1 000');
+  } else if (inputType.value === 'house' && target.value < 5000) {
+    target.setCustomValidity('Цена за ночь не может быть ниже 5 000');
+  } else if (inputType.value === 'palace' && target.value < 10000) {
+    target.setCustomValidity('Цена за ночь не может быть ниже 10 000');
+  } else if (target.value > 1000000) {
+    target.setCustomValidity('Цена за ночь не может быть более 1 000 000');
+  } else {
+    target.setCustomValidity('');
+  }
+});
+
+// Устанавливаем связь между типом жилья и ценой за ночь
+inputType.addEventListener('input', function (evt) {
+  var target = evt.target;
+
+  if (target.value === 'bungalo') {
+    inputPrice.setAttribute('min', 0);
+    inputPrice.placeholder = '0';
+  } else if (target.value === 'flat') {
+    inputPrice.setAttribute('min', 1000);
+    inputPrice.placeholder = '1 000';
+  } else if (target.value === 'house') {
+    inputPrice.setAttribute('min', 5000);
+    inputPrice.placeholder = '5 000';
+  } else {
+    inputPrice.setAttribute('min', 10000);
+    inputPrice.placeholder = '10 000';
+  }
+});
+
+// Запрещаем ручное редактирование поля адреса
+setDisabled(adFormAddressInput);
+
+// Связываем время заезда и выезда
+inputTimeIn.addEventListener('input', function () {
+  inputTimeOut.value = inputTimeIn.value;
+});
+
+inputTimeOut.addEventListener('input', function () {
+  inputTimeIn.value = inputTimeOut.value;
+});
+
